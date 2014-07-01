e8057b105783074fc3b855750ba87ff7701d5308
Mark S <mark@marksilver.net>
2014-07-01 09:15:46 -0400

    simplify SQL

--- app.rb
+++ app.rb
@@ -149,16 +149,8 @@ class AirQualityEgg < Sinatra::Base
       if META[key]["extras_hash"]["Default SQL"]
         META[key]["extras_hash"]["Default SQL"]
       else
-        if key == "aqe"
-          <<-EOS
-            SELECT site_table.id,site_table.created,site_table.description,site_table.location_domain,site_table.location_ele,site_table.location_exposure,site_table.location_lat,site_table.location_lon,site_table.title,site_table.status,
-            (SELECT data_table.datetime FROM \"#{META[key]["data_resource_id"]}\" data_table WHERE data_table.feed_id = site_table.id order by datetime desc LIMIT 1) AS last_datapoint
-            from \"#{META["aqe"]["site_resource_id"]}\" site_table
-          EOS
-        else
-          resource_id = META[key]["site_resource_id"].nil? ? META[key]["data_resource_id"] : META[key]["site_resource_id"]
-          "SELECT * from \"#{resource_id}\" #{META[key]["extras_hash"]["default_conditions"]}"
-        end
+        resource_id = META[key]["site_resource_id"].nil? ? META[key]["data_resource_id"] : META[key]["site_resource_id"]
+        "SELECT * from \"#{resource_id}\" #{META[key]["extras_hash"]["default_conditions"]}"
       end
     end
 
