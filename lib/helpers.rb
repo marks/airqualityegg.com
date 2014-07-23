c893221ad5ec15b10456f543bf72f1fa7da58f10
Mark S <mark@marksilver.net>
2014-07-23 15:52:58 -0400

    [#75444412] first pass for this feature

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -420,4 +420,23 @@ module AppHelpers
     end
   end
 
+def fetch_whole_socrata_dataset(endpoint, per_page = 1000, where_clause = nil, token = ENV['SOCRATA_APP_TOKEN'])
+  all_results = []
+  page = 0
+  request_url = "#{endpoint}&$limit=#{per_page}&$offset=#{per_page*page}"
+  request_url += "&$where=#{URI.encode(where_clause)}" if where_clause
+  puts "Fetching all results from #{request_url}"
+  page_results = JSON.parse(RestClient.get(request_url), {"X-App-Token" => token})
+  until page_results.empty?
+    all_results = all_results + page_results
+    puts "Added #{page_results.size} results from page #{page+1} for a total of #{all_results.size}"
+    page = page + 1
+    request_url = "#{endpoint}&$limit=#{per_page}&$offset=#{per_page*(page)}"
+    page_results = JSON.parse(RestClient.get(request_url), {"X-App-Token" => token})
+  end
+  puts "Collected a total of #{all_results.size} records"
+  return all_results
+end
+
+
 end
